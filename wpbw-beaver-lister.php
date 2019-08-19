6ec43fa3277947a04f0b1ba394057f713586de89
WP Beaver World <wpbeaverworld@gmail.com>
2019-08-19 09:08:40 +0530

    Update wpbw-beaver-lister.php

--- wpbw-beaver-lister.php
+++ wpbw-beaver-lister.php
@@ -5,13 +5,12 @@
  * 
  * @wordpress-plugin
  * Plugin Name:   Beaver Lister
- * Plugin URI:    http://www.wpbeaverworld.com/beaver-lister/
- * Description:   Showing page builder enabled pages and listing the used modules
+ * Plugin URI:    https://github.com/wpbeaverworld/wpbw-beaver-lister
+ * Description:   Checking page builder is activated on a page, post or cpt and making the list of the used modules
  * Author:        WP Beaver World (@wpbeaverworld)
- * Author URI:    http://www.wpbeaverworld.com
- * Support:       http://support.wpbeaverworld.com
+ * Author URI:    https://www.wpbeaverworld.com
  *   
- * Version:       1.0.5
+ * Version:       1.0.7
  *  
  * Text Domain:   beaver-lister
  * Domain Path:   languages  
@@ -29,13 +28,73 @@ if ( !defined( 'ABSPATH' ) ) {
   wp_die( __( "Sorry, you are not allowed to access this page directly." ) );
 }
 
-$bl_version = "1.0.5";
+$bl_version = "1.0.7";
 
 define( 'BL_DIR', plugin_dir_path( __FILE__ ) );
 define( 'BL_URL', plugins_url( '/', __FILE__ ) );
 define( 'BL_LANGUAGES_DIR', plugin_basename( dirname( __FILE__ ) ) . '/languages'  );
 
-add_action( 'plugins_loaded', 'wpbw_bl_plugin_loaded' );
+register_activation_hook( __FILE__, 'wpbw_activate' );
+		
+add_action( 'admin_init', 		'wpbw_plugin_deactivate' );
+add_action( 'switch_theme',		'wpbw_plugin_deactivate' );
+add_action( 'plugins_loaded', 	'wpbw_bl_plugin_loaded' );
+
+/**
+ * Activate plugin
+ */ 
+function wpbw_activate()
+{
+	if ( ! class_exists('FLBuilder') )
+	{
+		//* Deactivate ourself
+		deactivate_plugins( __FILE__ );
+		add_action( 'admin_notices', 			'wpbw_admin_notice_message' );
+		add_action( 'network_admin_notices', 	'wpbw_admin_notice_message' );
+		return;	
+	}
+}
+
+/**
+ * This function is triggered when the WordPress theme is changed.
+ * It checks if the Beaver Builder Plugin is active. If not, it deactivates itself.
+ */
+function wpbw_plugin_deactivate()
+{
+	if ( ! class_exists('FLBuilder') )
+	{
+		//* Deactivate ourself
+		deactivate_plugins( __FILE__ );
+		add_action( 'admin_notices', 			'wpbw_admin_notice_message' );
+		add_action( 'network_admin_notices', 	'wpbw_admin_notice_message' );
+	}
+}
+
+/**
+ * Shows an admin notice if you're not using the Beaver Builder Plugin.
+ */
+function wpbw_admin_notice_message()
+{
+	if ( ! is_admin() ) {
+		return;
+	}
+	else if ( ! is_user_logged_in() ) {
+		return;
+	}
+	else if ( ! current_user_can( 'update_core' ) ) {
+		return;
+	}
+
+	$error = __( 'Sorry, you can\'t use the Beaver Lister Plugin unless the Beaver Builder Plugin is active. The plugin has been deactivated.', 'beaver-lister' );
+
+	echo '<div class="error"><p>' . $error . '</p></div>';
+
+	if ( isset( $_GET['activate'] ) )
+	{
+		unset( $_GET['activate'] );
+	}
+}
+
 /**
  * Doing some activities when plugin loaded
  */
